d8b9d028d19683ea94d208b0417ec59e04f82734
Kuldeepbda <kuldeep.raj@bda.media>
2019-11-28 17:53:57 +0530

    New changes

--- laravelmongodb/resources/views/header.blade.php
+++ laravelmongodb/resources/views/header.blade.php
@@ -194,7 +194,7 @@
             <div class="row">
                 <div class="col-md-8 col-sm-8 col-xs-12">
                     <div class="ts-date">
-                        <i class="fa fa-calendar-check-o"></i>May 29, 2017
+                        <i class="fa fa-calendar-check-o"></i>{{date("F j, Y ")}}
                     </div>
                     <ul class="unstyled top-nav">
                         <li><a href="/top/page/aboutus">About us</a></li>
--- laravelmongodb/resources/views/writeus.blade.php
+++ laravelmongodb/resources/views/writeus.blade.php
@@ -19,66 +19,15 @@
 				<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 
 				<h3>Policy</h3>
-				<p>Lumbersexual meh sustainable Thundercats meditation kogi. Tilde Pitchfork vegan, gentrify minim elit semiotics non messenger bag Austin which roasted Lumbersexual meh sustainable Thundercats meditation kogi. Tilde Pitchfork vegan, gentrify minim elit semiotics non messenger bag Austin which roasted</p>
+				
 
-				<div class="widget contact-info">
+<div class="td-page-content"><p>This Privacy Policy applies to the Mediacorp group of companies collecting, using, disclosing, processing, and/or transferring out of Singapore, your personal data for one or more of the purposes described in this policy.</p><p>BDA Media Network respects your privacy. This document outlines our privacy policy for the Mediacorp group of companies.<br></p><p></p><p></p><h2>What personal data we collect and why we collect?</h2><h3>Comments</h3><p>When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor’s IP address and browser user agent string to help spam detection.</p><p>An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: https://automattic.com/privacy/. After approval of your comment, your profile picture is visible to the public in the context of your comment.</p><h3>Media</h3><p>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.</p><h3>Cookies</h3><p>If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment. These cookies will last for one year.</p><p>If you have an account and you log in to this site, we will set a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.</p><p>When you log in, we will also set up several cookies to save your login information and your screen display choices. Login cookies last for two days, and screen options cookies last for a year. If you select “Remember Me”, your login will persist for two weeks. If you log out of your account, the login cookies will be removed.</p><p>If you edit or publish an article, an additional cookie will be saved in your browser. This cookie includes no personal data and simply indicates the post ID of the article you just edited. It expires after 1 day.</p><h3>Embedded content from other websites</h3><p>Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.</p><p>These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p><h2>How long we retain your data</h2><p>If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p><p>For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.</p><h2>What rights you have over your data</h2><p>If you have an account on this site, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p><h2>Change to this Privacy Policy</h2><p>BDA Media Network may amend this policy from time to time. If we make any substantial changes in the way we use your personal information, we will notify you by posting an announcement on our platforms. You agree that such posting will constitute sufficient and adequate notice to you of the changes. You agree that your continued use of our products and services after such amendment will constitute your acceptance of and agreement to be bound by the modified policy.</p><h2>Release</h2><p>This policy is subject to any existing laws of the Republic of Singapore in relation to the protection of information and if required by any lawful authority for any legal purposes such as the enforcement of any laws or to prevent the breaching of any laws, we shall release all such information as is legally required by the requisite and lawful authority in accordance with the directions of the lawful court of the competent jurisdiction.<br></p><p></p><h2>Contact BDA Media Network</h2><p>BDA Media Network<br>Floor 6th, Caribe Plaza Building, 53 Palmeras Street, San Juan<br>Puerto Rico 00901<br>(Attention: Data Protection Officer)<br>Email:&nbsp;<a href="https://5vietnam.com/privacy-policy/ads@5vietnam.com">ads@5vietnam.com</a><font color="#007fac"><span style="caret-color: rgb(0, 127, 172);"></span></font></p><p>Please note that we may charge a reasonable fee for processing any data access request.</p></div>
 
-					<div class="contact-info-box">
-						<div class="contact-info-box-content">
-							<h4>News247 Webagency</h4>
-							<p>Hungry Center, 3021 Horizon Circle</p>
-						</div>
-					</div>
 
-					<div class="contact-info-box">
-						<div class="contact-info-box-content">
-							<h4>Mail Us</h4>
-							<p>contact@cornike.com.uk</p>
-						</div>
-					</div>
 
-					<div class="contact-info-box">
-						<div class="contact-info-box-content">
-							<h4>Call Us</h4>
-							<p>+253-480-8973</p>
-						</div>
-					</div>
+				
 
-				</div><!-- Widget end -->
-
-				<h3>Contact Form</h3>
-	    			<form id="contact-form" action="contact-form.php" method="post" role="form">
-	    				<div class="error-container"></div>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="form-group">
-									<label>Name</label>
-								<input class="form-control form-control-name" name="name" id="name" placeholder="" type="text" required>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<label>Email</label>
-									<input class="form-control form-control-email" name="email" id="email" 
-									placeholder="" type="email" required>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<label>Subject</label>
-									<input class="form-control form-control-subject" name="subject" id="subject" 
-									placeholder="" required>
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<label>Message</label>
-							<textarea class="form-control form-control-message" name="message" id="message" placeholder="" rows="10" required></textarea>
-						</div>
-						<div class="text-right"><br>
-							<button class="btn btn-primary solid blank" type="submit">Send Message</button> 
-						</div>
-					</form>
+			
 				</div><!-- Content Col end -->
 				@include('sidebar')
 			</div><!-- Row end -->
