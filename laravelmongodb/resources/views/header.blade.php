7789a1bf92701868e1f4417a5f3c0fd41d649a50
Kuldeepbda <kuldeep.raj@bda.media>
2019-12-09 19:42:08 +0530

    New changes for our site

--- laravelmongodb/resources/views/header.blade.php
+++ laravelmongodb/resources/views/header.blade.php
@@ -313,7 +313,7 @@
         <div  class="row ad-banner-header">
             <div class="col-md-12">
                 <!-- /21689237362/thoisu247-header-desktop -->
-                <div id='div-gpt-ad-1575891986270-0' style='width: 970px; height: 250px;'>
+                <div id='div-gpt-ad-1575891986270-0' style='height: 10px;'>
                     <script>
                         googletag.cmd.push(function() { googletag.display('div-gpt-ad-1575891986270-0'); });
                     </script>
